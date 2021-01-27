2cf40e9abe62d147d59e1bcb531a8e2d4f97369e
Oscar Hernandez <oh@wzrdgroup.com>
2021-01-27 08:01:49 -0500

    First draft

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,3 +10,57 @@ $("#toggle").click(function() {
     $("#menu").toggleClass("opened");
     $(".lv-header").toggleClass("openmenu");
 });
+
+$(document).ready(function(){
+    $(".home-select").click(function() {
+        $(this).toggleClass("lv-menu-item-active");
+        $(".mission-select").toggleClass("lv-menu-item-active");  
+        
+        $("#toggle").removeClass("open");
+        $("#nav-icon4").removeClass("open");
+        $(".menu-wrapper li a").removeClass("mobile-menu-active");
+        $(this).addClass("mobile-menu-active");
+        
+        $("#menu").removeClass("opened");
+        $(".lv-header").removeClass("openmenu");
+
+        $(".our-mission").animate({
+            opacity: 0,
+          }, 500, function() {    
+            $(".main-video").show();
+            $(".our-mission").hide();        
+            $(".main-video").animate({
+                opacity: 1,
+              }, 500, function() {                
+            });
+        });
+        
+
+    });
+
+    $(".mission-select").click(function() {
+        $(".home-select").toggleClass("lv-menu-item-active");
+        $(this).toggleClass("lv-menu-item-active");        
+
+        $("#toggle").removeClass("open");
+        $("#nav-icon4").removeClass("open");
+        $(".menu-wrapper li a").removeClass("mobile-menu-active");
+        $(this).addClass("mobile-menu-active");
+
+        $("#menu").removeClass("opened");
+        $(".lv-header").removeClass("openmenu");
+
+        $(".main-video").animate({
+            opacity: 0,
+          }, 500, function() {
+            $(".main-video").hide();
+            $(".our-mission").css("display","flex");
+
+            $(".our-mission").animate({            
+                opacity: 1,
+              }, 500, function() {
+            });
+        });
+        
+    });
+}); 
